fcc6d86a56a01854b2e357f1e45dee5e51599dc8
Eyoel Tesfu <eyoelytesfu@gmail.com>
2024-04-27 21:23:22 -0500

    Commentified some console logs

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -8,7 +8,7 @@ const auth = jwt.expressjwt({
   algorithms: ['HS256'],
   userProperty: 'payload'
 });
-console.log("auth (jwt): ", auth);
+// console.log("auth (jwt): ", auth);
 
 // Here, import the controllers we will route
 const authController = require('../controllers/authentication');
