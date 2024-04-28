ba7118775bedf4ac0504be043be3e3a74bd04c64
Eyoel Tesfu <eyoelytesfu@gmail.com>
2024-04-28 08:32:46 -0500

    Fixed undefined payload error

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -4,55 +4,30 @@ const Model = mongoose.model('trips');
 const jwt = require('jsonwebtoken');
 const User = mongoose.model('users');
 
-const getUser = (req, res, callback) => {
-  console.log("Trying to fetch user");
-  if (req.payload && req.payload.email) {
-    User.findOne({ email: req.payload.email }).exec((err, user) => {
+const getUser = async (req, res, callback) => {
+  const token = req.headers.authorization
+    ? req.headers.authorization.split(" ")[1]
+    : null;
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findOne({ email: decoded.email }).exec();
       if (!user) {
         return res.status(404).json({ message: "User not found" });
-      } else if (err) {
-        console.log(err);
-        return res.status(404).json(err);
       }
-      callback(req, res, user.name);
-    });
+      // callback(req, res, user.name);
+      return user;
+    } catch (err) {
+      console.error("JWT Error:", err.message);
+      res.status(401).json({ message: "Invalid token" });
+      return null;
+    }
   } else {
-    console.log("Couldn't find user since payload =", req.payload);
-    return res.status(404).json({ message: "Undefined payload" });
+    console.log("No token provided");
+    res.status(401).json({ message: "Unauthorized: No token provided" });
+    return null;
   }
 };
-
-// const getUser = async (req, res, callback) => {
-//   if (req.payload && req.payload.email) {
-//     try {
-//       const user = await User.findOne({ email: req.payload.email }).exec();
-//       if (!user) {
-//         return res.status(404).json({ message: "User not found" });
-//       }
-//       callback(req, res, user.name);
-//     } catch (err) {
-//       console.log(err);
-//       return res.status(404).json(err);
-//     }
-//   } else {
-//     return res.status(404).json({ message: "User not found" });
-//   }
-// };
-
-// const getUser = async (req) => {
-//   // console.log("payload". req.payload)
-//   // console.log("payload email". req.payload.email)
-//   if (req.payload && req.payload.email) {
-//     const user = await User.findOne({ email: req.payload.email }).exec();
-//     if (!user) {
-//       throw Error('User not found');
-//     }
-//     return user;
-//   } else {
-//     throw Error('User not found bc JWT');
-//   }
-// };
-
 
 // GET: /trips - lists all the trips
 // Regardless of outcome, response must include HTML status code
@@ -89,98 +64,39 @@ const tripsFindByCode = async (req, res) => {
   }
 
 }
-
-
-// const tripsAddTrip = async (req, res) => {
-//   // console.log(req.headers); // This will show you all headers, including the Authorization header.
-//   // console.log(req.payload); // This will show you the decoded JWT payload if it's being set correctly.
-//   try {
-//     const user = await getUser(req);
-//     const trip = await Trip.create({
-//       // Add the user's name or ID to the trip document if necessary
-//       // For example, as an owner or createdBy field:
-//       // owner: user._id, or createdBy: user.name,
-//       code: req.body.code,
-//       name: req.body.name,
-//       length: req.body.length,
-//       start: req.body.start,
-//       resort: req.body.resort,
-//       perPerson: req.body.perPerson,
-//       image: req.body.image,
-//       description: req.body.description,
-//     });
-//     return res.status(201).json(trip);
-//   } catch (err) {
-//     console.log(err);
-//     return res.status(400).json({ message: err.message });
-//   }
-// };
-
-
-// const tripsAddTrip = async (req, res) => {
-//   getUser(req, res, async (req, res, name) => {
-//     try {
-//       const trip = await Trip.create({
-//         code: req.body.code,
-//         name: req.body.name,
-//         length: req.body.length,
-//         start: req.body.start,
-//         resort: req.body.resort,
-//         perPerson: req.body.perPerson,
-//         image: req.body.image,
-//         description: req.body.description,
-//       });
-//       return res.status(201).json(trip);
-//     } catch (err) {
-//       return res.status(400).json(err);
-//     }
-
-function extractToken (req) {
-    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-        return req.headers.authorization.split(' ')[1];
-    } else if (req.query && req.query.token) {
-        return req.query.token;
-    }
-    return null;
-}
-
 const tripsAddTrip = async (req, res) => {
-  // req.payload = extractToken(req.headers.authorization[1]);
-  // req.payload.email = jwt.decode(req.payload);
-  // console.log("request header req.header = ", req.headers);
-  getUser(req, res, (req, res) => {
-    Trip.create(
-      {
-        code: req.body.code,
-        name: req.body.name,
-        length: req.body.length,
-        start: req.body.start,
-        resort: req.body.resort,
-        perPerson: req.body.perPerson,
-        image: req.body.image,
-        description: req.body.description,
-      },
-      (err, trip) => {
-        if (err) {
-          return res
-            .status(400) // bad request
-            .json(err);
-        } else {
-          return res
-            .status(201) // created
-            .json(trip);
-        }
-      }
-    );
-  });
-}; 
+  try {
+    const user = await getUser(req, res);
+    if (!user) {
+      return;
+    }
 
-// PUT: /trips/:tripCode - Adds a new Trip
-// Regardless of outcome, response must include HTML status code
-// and JSON message to the requesting client
+    const trip = await Trip.create({
+      code: req.body.code,
+      name: req.body.name,
+      length: req.body.length,
+      start: req.body.start,
+      resort: req.body.resort,
+      perPerson: req.body.perPerson,
+      image: req.body.image,
+      description: req.body.description,
+    });
+
+    return res.status(201).json(trip);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({ message: "Error creating trip" });
+  }
+};
+
 const tripsUpdateTrip = async (req, res) => {
-  getUser(req, res, (req, res) => {
-    Trip.findOneAndUpdate(
+  try {
+    const user = await getUser(req, res);
+    if (!user) {
+      // getUser will handle sending the response if there's no user
+      return;
+    }
+    const trip = await Trip.findOneAndUpdate(
       { code: req.params.tripCode },
       {
         code: req.body.code,
@@ -193,27 +109,26 @@ const tripsUpdateTrip = async (req, res) => {
         description: req.body.description,
       },
       { new: true }
-    )
-      .then((trip) => {
-        if (!trip) {
-          return res.status(404).send({
-            message: "Trip not found with code " + req.params.tripCode,
-          });
-        }
-        res.send(trip);
-      })
-      .catch((err) => {
-        if (err.kind === "ObjectId") {
-          return res.status(404).send({
-            message: "Trip not found with code " + req.params.tripCode,
-          });
-        }
-        return res
-          .status(500) // server error
-          .json(err);
+    ).exec(); // Use exec() to return a true Promise
+
+    if (!trip) {
+      return res.status(404).json({
+        message: "Trip not found with code " + req.params.tripCode,
       });
-  });
+    }
+    res.status(200).json(trip);
+  } catch (err) {
+    console.error(err);
+    if (err.kind === "ObjectId") {
+      res.status(404).json({
+        message: "Trip not found with code " + req.params.tripCode,
+      });
+    } else {
+      res.status(500).json(err);
+    }
+  }
 };
+
 
 module.exports = {
   tripsList,
